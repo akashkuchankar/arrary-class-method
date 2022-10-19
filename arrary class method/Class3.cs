e260bd01a1157677d884b9f7028ca89abe24beb2
akashkuchankar <kuchankarakash@gmail.com>
2022-10-19 09:00:10 -0700

    ararry

---
+++ arrary class method/Class3.cs
@@ -0,0 +1,40 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace arrary_class_method
+//{
+//    internal class Class3
+//    {
+//        static void Main(string[] args)
+//        {
+//            string str1 = "Akash";
+//            string str2 = " this is my name ";
+
+//            Console.WriteLine(str1.Length);
+//            Console.WriteLine(str2.Length);
+
+//            // string str3=string.Concat(str1," ", str2);
+//            string str3 = str1 + " " + str2;
+//            Console.WriteLine(str3);
+
+//            Console.WriteLine(str3.ToUpper());
+//            Console.WriteLine(str3.ToLower());
+
+//            char[] ch = str3.ToCharArray();
+//            foreach (var item in ch)
+//            {
+//                Console.WriteLine(item);
+//            }
+//            bool res1 = str3.StartsWith("Akash");
+//            bool res2 = str3.EndsWith("name");
+//            Console.WriteLine(res1);
+//            Console.WriteLine(res2);
+//            str2.Trim();
+//            str2.TrimStart();
+//            str2.TrimEnd();
+//        }
+//    }
+//}
---
+++ arrary class method/Class7.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrary_class_method
+{
+    internal class Class7
+    {
+        static void Main(string[] args)
+        {
+            int[,] numbers = new int[3, 4]
+            {
+                 {2,3,4,5 },
+                 { 3,5,7,8},
+                 { 3,5,7,9}
+            };
+            int nrow=numbers.GetLength(0);
+            int ncol=numbers.GetLength(1);
+            int lowest = numbers[0, 0];
+            for(int i = 0; i < nrow; i++)
+            {
+                for(int j = 0; j < ncol; j++)
+                {
+                    int num=numbers[i, j];
+                    if(num<lowest)
+                        lowest = num;
+                }
+            }
+            Console.WriteLine("lowest number is {0}",lowest);
+            Console.ReadLine();
+            
+        }
+    }
+}
