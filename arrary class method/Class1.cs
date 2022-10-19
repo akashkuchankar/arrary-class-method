3a19dc0aa308e58464b410e42e604c8f86ed9730
akashkuchankar <kuchankarakash@gmail.com>
2022-10-18 23:12:54 -0700

    Add project files.

---
+++ arrary class method/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrary_class_method
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 23, 56, 12, 89, 22 };
+           
+            Array.Sort(arr);
+            Array.Reverse(arr);
+            Console.WriteLine("sorted array");
+            foreach (int i in arr)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("reverse arrary");
+            foreach(int i in arr)
+            {
+                Console.WriteLine(i);
+            } 
+        }
+    }
+}
---
+++ arrary class method/Program.cs
@@ -0,0 +1,22 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace arrary_class_method
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int[] arr = new int[] { 23, 56, 12, 89, 22 };
+//            Array.Sort(arr);
+//            Console.WriteLine("sorted array");
+//            foreach (int i in arr)
+//            {
+//                Console.WriteLine(i);
+//            }
+//        }
+//    }
+//}
